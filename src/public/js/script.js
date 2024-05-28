e1e3b20860cefcd964db78d13097cbdd55def931
ilham_ath <ilham.athallah08@gmail.com>
2024-05-28 12:54:50 +0700

    new

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,3 +1,4 @@
+// contact
 const contactForm = document.getElementById("contact-form");
 
 contactForm.addEventListener("submit", function (e) {
@@ -16,4 +17,35 @@ contactForm.addEventListener("submit", function (e) {
         window.location.href = "/thankyou.html"
     })
     .catch((e) => alert("Error occured"));
+});
+
+// dark mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia(
+    '(prefers-color-scheme: dark)').matches)) {
+document.documentElement.classList.add('dark')
+} else {
+document.documentElement.classList.remove('dark')
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = 'light'
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = 'dark'
+
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem('theme')
+
+// Tambahkan event listener untuk tombol pengganti tema
+var themeToggleBtn = document.getElementById('theme-toggle');
+themeToggleBtn.addEventListener('click', function () {
+// Toggle kelas `dark` di elemen HTML
+document.documentElement.classList.toggle('dark');
+// Simpan preferensi tema pengguna di local storage
+if (document.documentElement.classList.contains('dark')) {
+    localStorage.setItem('theme', 'dark');
+} else {
+    localStorage.setItem('theme', 'light');
+}
 });
