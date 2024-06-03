a31ca2f25d63df62398f6a4d756005c9cd624d0e
ilham_ath <ilham.athallah08@gmail.com>
2024-06-03 10:47:23 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {
-      darkmode: 'class',
+    container: {
+      center: true,
+      padding: '16px',
     },
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        dark: '#0f172a',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1230px',
+      },
+      screens: {
+        '2xl': '1230px',
+      }
+    }
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+};
 
-  darkMode: 'selector',
-}
+
 
